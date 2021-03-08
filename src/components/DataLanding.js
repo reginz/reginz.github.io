f2c43daea8a5206826e1ad3c5aa0b28a088f9cfc
Zeynep Evecen <53823830+reginz@users.noreply.github.com>
2021-03-08 14:32:50 +0300

    Update DataLanding.js

--- src/components/DataLanding.js
+++ src/components/DataLanding.js
@@ -5,11 +5,9 @@ import './Dot.css';
 function DataLanding () {
         return (
             <div className="bg-radius-data">
-                {/* <div className="data-landing">
-                    <img src="../images/aixlanding_yellow.jpg" alt="AIX Landing Background Image"
-                    className="data-bg"/>
-                    <div className="data-container">
-                        <div className="dot-general1 dot1"></div>
+                
+                <div className="crowdsource-container">
+                <div className="dot-general1 dot1"></div>
                         <div className="dot-general2 dot2"></div>
                         <div className="dot-general1 dot3"></div>
                         <div className="dot-general3 dot4"></div>
@@ -19,34 +17,6 @@ function DataLanding () {
                         <div className="dot-general1  dot8"></div>
                         <div className="dot-general3  dot9"></div>
                         <div className="data-columns">
-                            <div className="column-data img-column">
-                                <img src="../images/robots_last.png" alt="Robots"/>
-
-                            </div>
-                            <div className="column-data banner-column">
-                                <p>
-                                    Collecting Large Data Sets While Providing Employment 
-                                    to People from Many Walks of Life
-                                </p>
-
-                            <div className="hr-wrapper">
-                                <hr></hr>
-                            </div>
-                                
-                            </div>
-                        </div>
-                    </div>
-                    
-                </div> */}
-                <div className="crowdsource-container">
-                <div className="dot-general2 dot2"></div>
-                        <div className="dot-general1 dot3"></div>
-                        <div className="dot-general3 dot4"></div>
-                        <div className="dot-general3  dot5"></div>
-                        <div className="dot-general3  dot6"></div>
-                        <div className="dot-general2  dot7"></div>
-                        <div className="dot-general1  dot8"></div>
-                        <div className="dot-general3  dot9"></div>
                     <div className="crowdsource-wrapper">
                     <div className="crowdsource-image desktop">
                             <img src="./images/robots_last.png"></img>
@@ -65,6 +35,7 @@ function DataLanding () {
                     </div>
                 </div>
 
+            </div>
             </div>
         );
     
