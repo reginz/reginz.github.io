8b4e3a5b2614859747efeabcfad535a9227f2847
Zeynep Evecen <53823830+reginz@users.noreply.github.com>
2021-03-09 20:35:52 +0300

    Add files via upload

---
+++ src/pages/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../../App.css';
+import HeroSection from '../HeroSection';
+import CreateFuture from '../CreateFuture';
+import Navbar from '../Navbar';
+
+function Home() {
+  return (
+    <>
+      
+      <HeroSection />
+      <CreateFuture />
+    </>
+  );
+}
+
+export default Home;
