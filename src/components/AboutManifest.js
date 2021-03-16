2163be4c04f3a3b077768510b877cc5602e4fc5d
Zeynep Evecen <53823830+reginz@users.noreply.github.com>
2021-03-16 19:36:18 +0300

    Update AboutManifest.js

--- src/components/AboutManifest.js
+++ src/components/AboutManifest.js
@@ -10,25 +10,25 @@ class AboutManifest extends Component {
                         Our Approach
                     </h5>
                     <p>
-                     We make AI accessible, serving Machine Learning related 
+                     <p>
+                    We make AI accessible, serving Machine Learning related 
                     technologies to individuals and businesses of all sizes 
                     quite flexible. Unlike common belief, we believe that AI 
                     will create more employment over time, and we are a part of
                      it. AI needs human culture to grow, and we collect the 
-                     required dataset while employing a wide variety of people. W
-                     e insist every product of AI must be an Intelligent Agent instea
-                     d of a tool. We must add features to let agents learn from any d
-                     ata they can access and relate to its mission. Currently, rebuil
-                     ding all tools as agents means a lot of time and effort. So our solution
+                     required dataset while employing a wide variety of people. We 
+                     insist every product of AI must be an Intelligent Agent instead of a tool. 
+                     We must add features to let agents learn from any data they can access and
+                     relate to its mission. Currently, rebuilding all tools as agents
+                      means a lot of time and effort. So our solution
                       is IAAAS or AAIAAS (Intelligent Agent as a Service or Automated AI as a Ser
-                      vice). We can compare IAAAS to adding the Frontal lobe to a reptilian brain we t
-                      
-                      ried to illustrate in our logo. Our ultimate goal is to tr
-                      ansfer human intelligence to non-biological 
+                      vice). We can compare IAAAS to adding the Frontal lobe to a reptilian brain we
+                       tried to illustrate in our logo. Our ultimate goal is to transfer
+                     human intelligence to non-biological 
                       hosts and create digital life forms. This
-                       seems the only way to intelligent continue to exp
-                       lore this rapidly growing universe and continue search
-                       ing for self.
+                       seems the only way to intelligent continue to explore
+                        this rapidly growing universe and continue searching for self.
+                    </p>
                     </p>
 
                 </div>
