00a16ce2d892f9c6ee6f11e128c73110efe3e492
Zeynep Evecen <53823830+reginz@users.noreply.github.com>
2021-03-21 21:38:44 +0300

    Update AboutManifest.js

--- src/components/AboutManifest.js
+++ src/components/AboutManifest.js
@@ -10,23 +10,43 @@ class AboutManifest extends Component {
                         Our Approach
                     </h5>
                     <p>
-                    We make AI accessible, serving Machine Learning related 
-                    technologies to individuals and businesses of all sizes 
-                    quite flexible. Unlike common belief, we believe that AI 
-                    will create more employment over time, and we are a part of
-                     it. AI needs human culture to grow, and we collect the 
-                     required dataset while employing a wide variety of people. We 
-                     insist every product of AI must be an Intelligent Agent instead of a tool. 
-                     We must add features to let agents learn from any data they can access and
-                     relate to its mission. Currently, rebuilding all tools as agents
-                      means a lot of time and effort. So our solution
-                      is IAAAS or AAIAAS (Intelligent Agent as a Service or Automated AI as a Ser
-                      vice). We can compare IAAAS to adding the Frontal lobe to a reptilian brain we
-                       tried to illustrate in our logo. Our ultimate goal is to transfer
-                     human intelligence to non-biological 
-                      hosts and create digital life forms. This
-                       seems the only way to intelligent continue to explore
-                        this rapidly growing universe and continue searching for self.
+                    Our mission is to implement 
+                    AI algorithms in a specific way
+                     so that people do not need
+                      to create their own 
+                      AI tools to benefit 
+                      from machine learning algorithms. We make AI accessible, 
+                      serving Machine Learning related technologies to individuals 
+                      and businesses of all sizes quite flexible. Unlike common belief, 
+                      we believe that AI will create more employment over time, and we are 
+                      a part of it. AI needs human culture to grow, and we collect the required
+                       dataset while employing a wide variety of people. We insist every
+                        product of AI must be an Intelligent Agent instead of a tool.
+                         We must add features to let agents learn from any data they can 
+                         access and relate to its tasks. Currently, rebuilding all tools 
+                         as agents involves a lot of time and effort. So our solution is
+                          IAAAS or AAIAAS (Intelligent Agent as a Service or Automated AI
+                           as a Service). We can compare IAAAS to adding the Frontal lobe
+                            to a reptilian brain, as we tried to illustrate in our logo. We
+                             create intelligent agents that will understand your businesses’
+                              flow and help you organize and optimize your products and processes.
+                               We will start a new era where artificial intelligence will be at your 
+                               service without you worrying about how to learn and use these algorithms.
+                                Our intelligent agents will be able to aid companies and businesses without 
+                                differentiating between domains. So far, the use of AI was at the hand of either
+                                 big tech companies or people who had the resources to create their own AI systems. 
+                                 They only used AI as a tool, which means that these algorithms only worked to solve 
+                                 specific problems in domains they were trained for. On the other hand, intelligent 
+                                 agents are a much stronger concept where AI can now take actions and make decisions
+                                  independently, without being given direct commands on its output. All you have to 
+                                  do is give an intelligent agent an environment of your own choice (regarding your 
+                                  business). The intelligent agent will start to learn and improve it. One of our
+                                   greatest goals is to benefit from intelligent agents using their maximum potential.
+                                    Automat-able tasks should be detected, implemented, and executed by intelligent
+                                     agents to maximize the efficiency of the business and decrease the need for any 
+                                     human labor. Our ultimate goal is to transfer human intelligence to non-biological 
+                                     hosts and create digital life forms. This seems the only way to intelligent continue
+                                      to explore this rapidly growing universe and continue searching for self.
                     </p>
 
 
