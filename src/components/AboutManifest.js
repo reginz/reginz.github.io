8c93be64c216b6d82b97899a5865fba27485a129
Zeynep Evecen <53823830+reginz@users.noreply.github.com>
2021-03-24 11:35:05 +0300

    Update AboutManifest.js

--- src/components/AboutManifest.js
+++ src/components/AboutManifest.js
@@ -20,7 +20,7 @@ class AboutManifest extends Component {
 	We will start a new era where artificial intelligence will be at your service 
             without you worrying about how to learn and use these algorithms. Our intelligent 
             agents will be able to aid companies and businesses without differentiating between 
-            domains. So far, the use of AI was at the hand of either big tech companies or 
+            domains.<br></br> So far, the use of AI was at the hand of either big tech companies or 
             people who had the resources to create their own AI systems. They only used AI as a tool, 
             which means that these algorithms only worked to solve specific problems in domains they were trained for.<br></br>
 	On the other hand, intelligent agents are a much stronger concept where AI can now take actions 
@@ -29,11 +29,12 @@ class AboutManifest extends Component {
             give an intelligent agent an environment of your own choice (regarding your business). The intelligent agent will start to learn and improve it.<br></br>
 	Currently, rebuilding all tools as agents involves a lot of time and effort. So our solution 
     is IAAAS or AAIAAS (Intelligent Agent as a Service or Automated AI as a Service). We can compare 
-    IAAAS to adding the Frontal lobe to a reptilian brain, as we tried to illustrate in our logo. 
+    IAAAS to adding the Frontal lobe to a reptilian brain, as we tried to illustrate in our logo. <br></br>
     We create intelligent agents that will understand your businesses’ flow and help you organize and optimize your products and processes.
 	One of our greatest goals is to benefit from intelligent agents using their maximum potential. 
     Automat-able tasks should be detected, implemented, and executed by intelligent agents to 
-    maximize the business’s efficiency and decrease the need for any human labor. Our ultimate 
+    maximize the business’s efficiency and decrease the need for any human labor. 
+    <br></br>Our ultimate 
     goal is to transfer human intelligence to non-biological hosts and create digital life forms. 
     This seems the only way to intelligent continue to explore this rapidly growing universe and continue searching for self.
                     </p>
