833d3ec453b1f258a2e849cff9619989c16fe9f7
Zeynep Evecen <53823830+reginz@users.noreply.github.com>
2021-03-25 17:27:51 +0300

    Update AboutManifest.js

--- src/components/AboutManifest.js
+++ src/components/AboutManifest.js
@@ -9,42 +9,41 @@ class AboutManifest extends Component {
                     <h5>
                         Our Approach
                     </h5>
-                    <p>
-                  Our mission is to make AI accessible, serving Machine Learning related 
+                    <article>
+                    <p>Our mission is to make AI accessible, serving Machine Learning related 
             technologies to individuals and businesses of all sizes quite flexible so that 
             people do not need to create their own AI tools to benefit from machine learning 
-            algorithms.<br></br>
-	        Unlike common belief, we believe that AI will create more employment over time. 
-            We know that AI related  
-             <a href="https://www.prnewswire.com/news-releases/artificial-intelligence-is-creating-jobs-dun--bradstreet-survey-finds-300774141.html" target="_blank">  job
-              creation reached 40% </a>among organizations according to 
-            Dun and Bradstreet, and we are a part of it.
+            algorithms.<br></br></p>
+	        
+            <p>
+            Unlike common belief we believe that AI will create more employment over time and it already <a href="https://www.prnewswire.com/news-releases/artificial-intelligence-is-creating-jobs-dun--bradstreet-survey-finds-300774141.html" target="_blank"> reached 40% </a>among organizations.
             AI needs human culture
               to grow, and we create the 
-            required datasets while employing a wide variety of people.<br></br>
+            required datasets while employing a wide variety of people.<br></br></p>
+            <p>
 	We will start a new era where artificial intelligence will be at your service 
             without you worrying about how to learn and use these algorithms. Our intelligent 
             agents will be able to aid companies and businesses without differentiating between 
-            domains.<br></br> So far, the use of AI was at the hand of either big tech companies or 
-            people who had the resources to create their own AI systems. According to CompTIA, <a href="https://connect.comptia.org/content/research/emerging-business-opportunities-in-ai" target="_blank">only 29% of companies use AI</a> regularly.
+            domains.<br></br></p><p>So far, the use of AI was at the hand of either big tech companies or 
+            people who had the resources to create their own AI systems. It is estimated that <a href="https://connect.comptia.org/content/research/emerging-business-opportunities-in-ai" target="_blank">only 29% of companies use AI</a> regularly.
             Because they only used AI as a tool, 
-            which means that these algorithms only worked to solve specific problems in domains they were trained for.<br></br>
-	On the other hand, intelligent agents are a much stronger concept where AI can now take actions 
+            which means that these algorithms only worked to solve specific problems in domains they were trained for.<br></br></p> 
+	<p>On the other hand, intelligent agents are a much stronger concept where AI can now take actions 
             and make decisions independently, without being given direct commands on its output. 
             They must learn from any data they can access and relate to its tasks. All you have to do is 
             give an intelligent agent an environment of your own choice (regarding your business). The intelligent agent will start to learn and improve it.<br></br>
 	Currently, rebuilding all tools as agents involves a lot of time and effort. So our solution 
     is IAAAS or AAIAAS (Intelligent Agent as a Service or Automated AI as a Service). We can compare 
-    IAAAS to adding the Frontal lobe to a reptilian brain, as we tried to illustrate in our logo. <br></br>
+    IAAAS to adding the Frontal lobe to a reptilian brain, as we tried to illustrate in our logo. <br></br></p><p>
     We create intelligent agents that will understand your businesses’ flow and help you organize and optimize your products and processes.
 	One of our greatest goals is to benefit from intelligent agents using their maximum potential. 
     Automat-able tasks should be detected, implemented, and executed by intelligent agents to 
     maximize the business’s efficiency and decrease the need for any human labor. 
-    <br></br>Our ultimate 
+    <br></br></p><p>Our ultimate 
     goal is to transfer human intelligence to non-biological hosts and create digital life forms. 
     This seems the only way to intelligent continue to explore this rapidly growing universe and continue searching for self.
                     </p>
-
+                    </article>
 
                 </div>
                 
