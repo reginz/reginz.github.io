1c6be97c06180a59abf57ef73a52f5baddc4877e
Zeynep Evecen <53823830+reginz@users.noreply.github.com>
2021-04-06 19:25:56 +0300

    Update Email.js

--- src/components/Email.js
+++ src/components/Email.js
@@ -3,14 +3,13 @@ import './Email.css';
 import Mailchimp from 'react-mailchimp-form';
 
 class Email extends Component {
-    render() {
-        return (
-          <div className="wrap-email">
-            <h2>Join Our <span>Mailing List</span></h2>
-            <p>We'll keep you informed about our products and development.</p>
-            <Mailchimp
-        action='https://aiforexistence.us1.list-manage.com/subscribe/post?u=dcbf0dfb733d51f91d9084b87&amp;id=5fe2970b32
-         method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form'
+  render() {
+    return (
+      <div className="wrap-email">
+        <h2>Join Our Mailing List</h2>
+        <p>We'll keep you informed about our products and development.</p>
+        <Mailchimp
+        action='https://aiforexistence.us1.list-manage.com/subscribe/post?u=dcbf0dfb733d51f91d9084b87&amp;id=5fe2970b32'
         fields={[
           {
             name: 'EMAIL',
@@ -19,23 +18,11 @@ class Email extends Component {
             required: true
           }
         ]}
-        messages = {
-            {
-              sending: "Sending...",
-              success: "Thank you for subscribing!",
-              error: "An unexpected internal error has occurred. Please try again.",
-              empty: "You must write an e-mail.",
-              duplicate: "Too many subscribe attempts for this email address",
-              button: "Join"
-            }
-          }
-          // Add a personalized class
-          className='email'
-        
+        className="email"
         />
         </div>
-        );
-    }
+    );
+    
+  }
 }
-
 export default Email;
