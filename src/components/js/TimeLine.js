c4457495efd0bd55acf963a48a83af16d3658282
Zeynep Evecen <53823830+reginz@users.noreply.github.com>
2021-04-30 14:16:37 +0300

    Update TimeLine.js

--- src/components/js/TimeLine.js
+++ src/components/js/TimeLine.js
@@ -12,52 +12,46 @@ class TimeLine extends Component {
 			</div>
 
 			<div class="cd-timeline-content">
-				<h2>Penta Consulting</h2>
-        <div class="timeline-content-info">
-          <span class="timeline-content-info-title">
-            <i class="fa fa-certificate" aria-hidden="true"></i>
-            Front End Developer
-          </span>
-          <span class="timeline-content-info-date">
-            <i class="fa fa-calendar-o" aria-hidden="true"></i>
-            June 2016 - Present
-          </span>
-        </div>
-				<p>Working alongside the designer team implementing the designs, also developing custom solutions to address team necessities.</p>
-        <ul class="content-skills">
-        <li>HTML5</li>
-        <li>CSS3</li>
-        <li>JavaScript</li>
-        <li>jQuery</li>
-        <li>Wordpress</li>
-        </ul>
+				<h2>2021 Q1</h2>
+
+				<p>Foundation of the company.<br></br>
+R&D studies.<br></br>
+Product design.
+
+</p>
+        
 			</div> 
 		</div> 
 
 		<div class="cd-timeline-block">
 			<div class="cd-timeline-img cd-movie">
 			</div> 			<div class="cd-timeline-content">
-				<h2>Title of section 2</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum aut hic quasi placeat iure tempora laudantium ipsa ad debitis unde?</p>
-				<span class="cd-date">Jan 18</span>
+				<h2>2021 Q2</h2>
+				<p>Initiating developement of the agent as a service modules.<br></br>
+Explaining our ideas about the future of AI through our blog.<br></br>
+Forming our community.<br></br>
+
+</p>
+				
 			</div> 
 		</div> 
 
 		<div class="cd-timeline-block">
 			<div class="cd-timeline-img cd-picture">
 			</div> 			<div class="cd-timeline-content">
-				<h2>Title of section 3</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, obcaecati, quisquam id molestias eaque asperiores voluptatibus cupiditate error assumenda delectus odit similique earum voluptatem doloremque dolorem ipsam quae rerum quis. Odit, itaque, deserunt corporis vero ipsum nisi eius odio natus ullam provident pariatur temporibus quia eos repellat consequuntur perferendis enim amet quae quasi repudiandae sed quod veniam dolore possimus rem voluptatum eveniet eligendi quis fugiat aliquam sunt similique aut adipisci.</p>
-				<span class="cd-date">Jan 24</span>
+				<h2> 2021 Q3</h2>
+				<p>API & integration of our service.<br></br>
+Releasing closed & open betas.</p>
+			
 			</div> 
 		</div> 
 
 		<div class="cd-timeline-block">
 			<div class="cd-timeline-img cd-location">
 			</div> 			<div class="cd-timeline-content">
-				<h2>Title of section 4</h2>
+				<h2>2021 Q4</h2>
 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum aut hic quasi placeat iure tempora laudantium ipsa ad debitis unde? Iste voluptatibus minus veritatis qui ut.</p>
-				<span class="cd-date">Feb 14</span>
+		
 			</div> 
 		</div> 
 
@@ -66,20 +60,45 @@ class TimeLine extends Component {
 			</div> 
 
 			<div class="cd-timeline-content">
-				<h2>Title of section 5</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum.</p>
-				<span class="cd-date">Feb 18</span>
+				<h2>2022 Q1</h2>
+				<p>Shipping our service.
+
+</p>
 			</div> 
 		</div> 
 
 		<div class="cd-timeline-block">
 			<div class="cd-timeline-img cd-movie">
 			</div> 			<div class="cd-timeline-content">
-				<h2>Final Section</h2>
-				<p>This is the content of the last section</p>
-				<span class="cd-date">Feb 26</span>
+				<h2>2022 Q2</h2>
+				<p>Developement of crowd-sourced data collection app.
+
+</p>
+				
 			</div> 
 		</div> 
+        <div class="cd-timeline-block">
+			<div class="cd-timeline-img cd-movie">
+			</div> 			<div class="cd-timeline-content">
+				<h2>🌍 Near Future</h2>
+				<p>More details to be announced soon...
+
+</p>
+				
+			</div> 
+		</div> 
+        <div class="cd-timeline-block">
+			<div class="cd-timeline-img cd-movie">
+			</div> 			<div class="cd-timeline-content">
+				<h2>🚀 Long Term</h2>
+				<p>Reaserching context free unsupervised model transfer.<br></br>
+Augumenting human-computer interface with agent architecture.
+
+</p>
+				
+			</div> 
+		</div> 
+        
 	</section>  
      </div>
        
