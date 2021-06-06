2f25a6dcb5d1f0dbfb0710b131917af4d3642b9b
Zeynep Evecen <zeynepevecen@gmail.com>
2021-06-06 23:14:27 +0300

    git repo

---
+++ src/components/js/Construction.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import '../css/Construction.css';
+
+function Construction()  {
+ 
+        return (
+            <div>
+                <div className="temp">
+                    
+                    <h1> 
+                    
+                        This Page Is Comming Soon!
+                       
+                    </h1>
+                </div>
+                
+            </div>
+        );
+    
+}
+
+export default Construction;
