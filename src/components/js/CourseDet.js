7b11060bcb10679daf3f97f5d7d93aa6ff19974e
Zeynep Evecen <53823830+reginz@users.noreply.github.com>
2021-11-08 16:17:18 +0300

    course content modified

--- src/components/js/CourseDet.js
+++ src/components/js/CourseDet.js
@@ -2,7 +2,7 @@ import React from 'react';
 import AI from '../css/image/robot.png'
 import Man from '../css/image/man.png';
 import { Popup /* or Slider */ } from 'react-typeform-embed';
-
+import '../css/CourseDet.css'
 function CourseDet(props) {
     return (
         <div style={{
@@ -28,7 +28,7 @@ function CourseDet(props) {
                     justifyContent:"center"
 
                 }} id="course-col-wrapper">
-                    <div style={{width:"20%"}} className="course-img-wrapper">
+                    <div style={{width:"50%"}} className="course-img-wrapper">
                      <img src={AI} style={{width:"100%"}}></img>
                      </div>
                     <div style={{
@@ -38,14 +38,20 @@ function CourseDet(props) {
                     }}
                     className="course-col">
                         <h2>1. What is AI?</h2>
-                        <p>Terminology </p>
-                        <p>
-                            What Deep Learning can and can't do and the other AI trends </p>
+                        <p className="subsection"><b>1.1 Foundation of AI </b></p>
+                        <p className="subsubsection">1.1.1 AI in philosophy, mathematics, economics, neuroscience, psychology, computer science, linguistics </p>
+                        <p className="subsubsection">1.1.2 Control theory and cybernetics</p>
+                        <p className="subsubsection">1.1.3Yapay Zeka, Agents, Machine Learning, Deep Learning, Probabilistic reasoning and models, Neural Nets, Big Data</p>
+                        <p className="subsection"><b> 1.2 What Deep Learning can and can't do and the other AI trends </b></p>
+                        <p className="subsubsection">1.2.1 Supervised / Unsupervised, Reinforcement learning, Symbolic reasoning, evolutionary algorithms</p>
                         <br></br><br></br>
                         <h2>2. AI Projects </h2>
-                        <p>Workflow in ML(Machine Learning) and DS(Data Science) projects </p>
-                        <p>Selection and requirements of Data and its Teams </p>
-                        <p>No-Code solutions for AI </p>
+                        <p className="subsection"><b>2.1 Workflow in ML(Machine Learning) and DS(Data Science) projects </b></p>
+                        <p className="subsubsection"> 2.1.1 Data collection, model building, testing and iteration, edge and cloud implementation, production ready models</p>
+                        <p className="subsection"><b> 2.2 Selection and requirements of Data and its Teams </b></p>
+                        <p className="subsubsection">2.2.1 Data engineering, ML engineering, Data Science, MLOps </p>
+                        <p className="subsection"><b>2.3 No-Code solutions for AI </b></p>
+                        <p className="subsubsection">2.3.1 Hugging face, Google Cloud, MonkeyLearn</p>
                     </div>
                    
                 </div>
@@ -67,21 +73,27 @@ function CourseDet(props) {
                     }}
                     className="course-col">
                         <h2>3. On-premises AI  </h2>
-                        <p>The role of AI projects and teams within the company </p>
-                        <p>The company's AI strategy and AI transformation </p>
-                        <p>AI main application areas </p>
-                        <p>Practical AI applications </p>
+                        <p className="subsection"><b>3.1 The role of AI projects and teams within the company </b></p>
+                        <p className="subsubsection">3.1.1 Projections examples and importance, Outlining AI strategy </p>
+                        <p className="subsection"><b>3.2 The company's AI strategy and AI transformation </b></p>
+                        <p className="subsection"><b>3.3 AI main application areas </b></p>
+                        <p className="subsubsection">3.3.1 NLP, Computer Vision, Speech Recognition</p>
+                        <p className="subsection"><b>3.4 Practical AI applications </b></p>
+                        <p className="subsubsection">3.4.1 eCommerce, Customer service, Recruiting, Predictive and Prescriptive Analytics</p>
                         <br></br><br></br>
                         <h2>4. AI and Ethics  </h2>
-                        <p>What is AI really? Could it serve a bad purpose? </p>
-                        <p>AI and economic effects </p>
-                        <p>AI and employment </p>
+                        <p className="subsection"><b>4.1 What is AI really? Could it serve a bad purpose? </b></p>
+                        <p className="subsubsection">4.1.1 Bias, Security and privacy, Robot rights, AI safety and friendly AI, Opening Blackbox</p>
+                        <p className="subsection"><b>4.2 AI and economic effects </b></p>
+                        <p className="subsubsection">4.2.1 Employment, automation, a world without work</p>
+                        <p className="subsection"><b>4.3 Future of AI</b></p>
+                        <p className="subsubsection">4.3.1 Artificial General Intellignece</p>
                         <br></br><br></br>
 
                         <h2>5. Conclusion and closing   </h2>
                     </div>
 
-                    <div style={{width:"30%"}}className="course-img-wrapper">
+                    <div style={{width:"50%"}}className="course-img-wrapper">
                     <img src={Man} style={{width:"100%"}}></img>
                     </div>
 
